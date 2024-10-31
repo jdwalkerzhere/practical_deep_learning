0e865ce0a58331e8e08477d658ef351661b020d1
Jesse Walker <jesse@Jesses-MBP.lan>
2024-10-31 11:42:28 -0400

    little bit more of lesson 1

--- part_one_lesson_one/main.py
+++ part_one_lesson_one/main.py
@@ -13,7 +13,12 @@ def search_images(keywords, max_images=200):
 def main():
     # Checking image search
     urls = search_images(keywords='bird photos', max_images=1)
-    print(urls[0])
+    dest = 'bird.jpg'
+    download_url(urls[0], dest, show_progress=False)
+    im = Image.open(dest)
+    im.thumbnail(256,256).save(dest)
+    download_url(search_images('forest photos', max_images=1)[0], 'forest.jpg', show_progress=False)
+    Image.open('forest.jpg').to_thumb(256,256)
 
 
 if __name__ == '__main__':
