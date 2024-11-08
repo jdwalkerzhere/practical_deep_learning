88389a7b590593e1c3a8babb8d7af53bc56577c7
jdwalkerzhere <jdwalkerzhere@comcast.net>
2024-11-08 12:26:51 -0500

    added the ability to build and export a model for any number of keywords, TODO figure out how to run loaded model

--- part_one_lesson_one/main.py
+++ part_one_lesson_one/main.py
@@ -1,3 +1,4 @@
+from sys import argv
 from time import sleep
 
 from duckduckgo_search import DDGS
@@ -10,15 +11,70 @@ def search_images(keywords, max_images=200):
     return L(DDGS().images(keywords, max_results=max_images)).itemgot('image')
 
 
+def populate_image_directories(searches, path, sleep_time, mode):
+    path = Path(path)
+    max_images = 1 if mode == 'sample' else 200
+
+    for o in searches:
+        message = f'Adding {o} sample image' if mode == 'sample' else f'Populating {o} image directory'
+        print(message)
+        dest = (path/o)
+        dest.mkdir(exist_ok=True, parents=True)
+        download_images(dest, urls=search_images(f'{o} photo', max_images))
+        if mode == 'sample':
+            continue
+
+        sleep(sleep_time)
+        download_images(dest, urls=search_images(f'{o} sun photo', max_images))
+        sleep(sleep_time)
+        download_images(dest, urls=search_images(f'{o} shade photo', max_images))
+        resize_images(path/o, max_size=400, dest=path/o)
+
+    return path  # For latter verification and DataBlock Construction
+
+
+def failed_images_count(path):
+    failed = verify_images(get_image_files(path))
+    failed.map(Path.unlink)
+    return len(failed)
+
+
+def learner_generator(keywords, path, sleep_time=10, export_name='temp_name'):
+    print(f'Generating a learner to distiguish between {" and ".join(keywords)}')
+    path = populate_image_directories(keywords, path, sleep_time, 'dev')
+    print(f'Number of failed images: {failed_images_count(path)}')
+
+    print('Populating DataBlock')
+    dls = DataBlock(
+        blocks=(ImageBlock, CategoryBlock),
+        get_items=get_image_files,
+        splitter=RandomSplitter(valid_pct=0.2, seed=42),
+        get_y=parent_label,
+        item_tfms=[Resize(192, method='squish')]
+    ).dataloaders(path)
+
+    print('Setting up Vision Learner')
+    learn = vision_learner(dls, resnet18, metrics=error_rate)
+    print('Fine Tuning Learner')
+    learn.fine_tune(3)
+    print(f'Exporting learner model to {export_name}.pkl')
+    learn.export(f'{export_name}.pkl')
+
+
 def main():
-    # Checking image search
-    urls = search_images(keywords='bird photos', max_images=1)
-    dest = 'bird.jpg'
-    download_url(urls[0], dest, show_progress=False)
-    im = Image.open(dest)
-    im.thumbnail(256,256).save(dest)
-    download_url(search_images('forest photos', max_images=1)[0], 'forest.jpg', show_progress=False)
-    Image.open('forest.jpg').to_thumb(256,256)
+    # User only provides keywords
+    keywords = argv[1:]
+
+    if not keywords:
+        raise RuntimeError('No arguments provided: Please add desired image types you would like a trained model for')
+
+    path = '_'.join(keywords)
+    
+    # populate a sample directory
+    populate_image_directories(keywords, 'samples', 0, 'sample') 
+
+    learner_generator(keywords, path, 10, path)
+    print(f'{path} Model Generated.')
 
 
 if __name__ == '__main__':
---
+++ part_one_lesson_one/predict.py
@@ -0,0 +1,46 @@
+from os import listdir, path
+from fastcore.all import *
+from fastai.vision import *
+from fastai.learner import load_learner
+from PIL import Image
+
+
+def main():
+    selected_model = None
+    while selected_model == None:
+        present_models = {i: f for i, f in enumerate(listdir('.'), start=1) if path.isfile(f) and f.endswith('pkl')}
+
+        print("Which model do you want to use? Select with number.")
+        for ind, model in present_models.items():
+            print(f'{ind}. {model}')
+
+        user_input = input()
+        
+        try:
+            user_input = int(user_input) 
+        except ValueError:
+            print(f'Cannot convert {user_input} to integer. Please type a number')
+            continue
+
+        assert isinstance(user_input, int)
+
+        selected_model = present_models.get(user_input, None)
+        if not selected_model:
+            print(f'Number {user_input} not present in the list. Please type a number that is present')
+            continue
+
+    print(f'Loading {selected_model} model')
+    learner = load_learner(selected_model)
+
+    image_path = input('What file do you want predictions for? Put in the path to the file\n')
+    if not path.exists(image_path):
+        raise RuntimeError(f'No path {image_path} exists')
+
+    image = Image.open(image_path) 
+    is_a, _, probs = learner.predict(image)
+    print(f'This image is a {is_a}')
+    print(f'Probability: {probs}')
+        
+
+if __name__ == '__main__':
+    main()
